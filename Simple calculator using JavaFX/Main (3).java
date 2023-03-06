0dd556734c14be909822a9ff1997e4537916d726
Divya Kulkarni <103066357+divya-kulkarni99@users.noreply.github.com>
2023-03-06 12:16:35 -0800

    Add files via upload

---
+++ Simple calculator using JavaFX/Main (3).java
@@ -0,0 +1,27 @@
+//Create four function calculator using JavaFX
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene = new Scene(root,400,400);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Simple Calculator");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
